7f9c0e8a02d988c0d441a6cd1895f7ee1d4958cb
luizagostinho <luizagostinhoms@gmail.com>
2019-08-28 15:11:29 -0300

    Implementação de leitura de cidade de arquivo txt, com informações separadas por ;

--- src/RunnerApp.java
+++ src/RunnerApp.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,7 +25,7 @@ public class RunnerApp {
 			System.out.println(cidade);
 		}
 		
-		Integer menu = 5;
+		Integer menu = 6;
 		Scanner valorMenu = new Scanner(System.in);
 		
 		while (menu != 0) {
@@ -39,6 +44,9 @@ public class RunnerApp {
 			case 4:
 				Relatorios.mediaAcidentesCidadesMenor2000Veiculos(cidades);
 				break;
+			case 5:
+				imprimirCidades(cidades);
+				break;
 			case 0:
 				System.out.println("\nSaindo da aplicação");
 				break;
@@ -50,6 +58,12 @@ public class RunnerApp {
 		
 	}
 
+	private static void imprimirCidades(List<Cidade> cidades) {
+		for (Cidade cidade : cidades) {
+			System.out.println(cidade);
+		}
+		
+	}
 
 	private static void imprimirMenuDeInformacoes() {
 		System.out.println("\n\n----RELATÓRIOS----- \n\n");
@@ -58,14 +72,37 @@ public class RunnerApp {
 	    System.out.println("\n2 - DESCOBRIR CIDADE COM MENOR NUMERO DE ACIDENTES: ");
 	    System.out.println("\n3 - MÉDIA DE VEICULOS ENTRE AS CIDADES: ");
 	    System.out.println("\n4 - MÉDIA DE ACIDENTES ENTRE AS CIDADES COM MENOS DE 2000 VEICULOS: ");
+	    System.out.println("\n5 - LISTAR TODAS AS CIDADE: ");
 	    System.out.println("\n0 - SAIR DA APLICAÇÃO: ");
-	    System.out.println("\nopcaçõ:");
+	    System.out.println("\nopção:");
 	}
 
 
 	private static List<Cidade> popularDadosCidade() {
 		
-		//TODO desenvolver lógica para ler cidades de arquivo. CIDADE.txt
-		return null;
+		List<Cidade> cidades = new ArrayList<Cidade>();
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader("CIDADE.txt"));
+			while(br.ready()){
+			
+				String linha = br.readLine();
+				Cidade cidade = new Cidade();
+				
+				String[] linhaCidade = linha.split(";");
+				cidade.setCodigoCidade(Integer.valueOf(linhaCidade[0]));
+				cidade.setNomeCidade(linhaCidade[1]);
+				cidade.setQuantidadeVeiculo(Integer.valueOf(linhaCidade[2]));
+				cidade.setQuantidadeVitimas(Integer.valueOf(linhaCidade[3]));
+				
+				cidades.add(cidade);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return cidades;
 	}
 }
